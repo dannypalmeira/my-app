26f2e8df676b93073e20bb21e3a0679f3e1023a2
Danny Palmeira <99752093+dannypalmeira@users.noreply.github.com>
2024-03-14 17:50:45 +0100

    Add files via upload

--- pages/resultado.js
+++ pages/resultado.js
@@ -1,12 +1,35 @@
-import SearchBar from '@/components/SearchBar';
-import React from 'react';
+import { useEffect, useState } from 'react';
+import FilmeCard from '@/components/FilmeCard';
+import { useRouter } from 'next/router';
+import { supabase } from '@/lib/initSupabase';
 
+const ResultadoPage = () => {
+  const router = useRouter();
+  const [searchResults, setSearchResults] = useState([]);
+  const [searched, setSearched] = useState(false);
+  const { busca } = router.query; 
+  
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      if (busca) {
+        const { data } = await supabase
+          .from("filmes")
+          .select("*")
+          .filter("titulo", "ilike", `%${busca}%`);
+        setSearchResults(data);
+        setSearched(true);
+      }
+    };
+    fetchSearchResults();
+  }, [busca]);
 
-export default function MoviePage () {
-    return (
-      <div>
-        <h1>Movie Details</h1>
-        <SearchBar />
-      </div>
-    );
-  };
+  return (
+    <div>
+      <h1>Resultado da busca por "{busca}"</h1>
+      <FilmeCard data={searchResults} />
+      {searched && searchResults.length === 0 && <p>No results found.</p>}
+    </div>
+  );
+};
+
+export default ResultadoPage;
