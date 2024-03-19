05baaae79c0df287e261a03f89abe536aa0fbe79
Danny Palmeira <99752093+dannypalmeira@users.noreply.github.com>
2024-03-19 16:50:25 +0100

    Add files via upload

---
+++ pages/api/submitForm.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { nome, indicacao } = req.body;
+
+    console.log('Data received:', { nome, indicacao });
+
+    try {
+      const response = await axios.post('https://script.google.com/macros/s/AKfycbwmMFVDD21w_6iL2K6-LqmOsffLoT0DvLI3aozX2iNdfr1tOlEgTRjS9pQdZoa4V_bV/exec', 
+        new URLSearchParams({
+            'nome': nome,
+            'indicacao': indicacao,
+          })
+          );
+      
+      if (response.status === 200) {
+        res.status(200).json({ success: true });
+      } else {
+        throw new Error('Failed to submit form data to Google Sheets');
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
